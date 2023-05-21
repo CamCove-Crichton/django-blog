1d3084fb5e5bc3f63a207ee564cbfecd19452e98
CamCove-Crichton <>
2023-05-21 17:43:02 +0000

    Create superuser and install summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cc-i-blog-therefore-i-am.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['cc-i-blog-therefore-i-am.herokuapp.com', '8000-camcove-crichton-django-5up6qsu5a2.us2.codeanyapp.com']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
