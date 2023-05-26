708e506198543dc1d9681f92600bc8b655bb2a44
CamCove-Crichton <>
2023-05-26 05:36:46 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['cc-i-blog-therefore-i-am.herokuapp.com', '8000-camcove-crichton-django-5up6qsu5a2.us2.codeanyapp.com']
 
